b1afda82b5d8fc734d019a2d5e138c00a847dbac
acaleza <adrian.calvo1@gmail.com>
2023-05-23 23:41:37 +0200

    Stream operation - distinct(), count() y sorted()

--- src/main/java/streams/StreamsFlatMapExample.java
+++ src/main/java/streams/StreamsFlatMapExample.java
@@ -13,15 +13,44 @@ public class StreamsFlatMapExample {
 
         List<String> studentActivities = StudentDataBase.getAllStudents()
                 .stream()
-                .map(Student::getActivities)
-                .flatMap(List::stream)
+                .map(Student::getActivities) //Stream<List<String>>
+                .flatMap(List::stream) //Stream<String>
                 .collect(toList());
 
         return studentActivities;
 
     }
 
+    public static List<String> printUniqueStudentActivities() {
+
+        List<String> studentActivities = StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getActivities)
+                .flatMap(List::stream)
+                .distinct()
+                .sorted()
+                .collect(toList());
+
+        return studentActivities;
+
+    }
+
+    public static long getStudentActivitiesCount() {
+
+        long totalActivities = StudentDataBase.getAllStudents()
+                .stream()
+                .map(Student::getActivities)
+                .flatMap(List::stream)
+                .distinct()
+                .count();
+
+        return totalActivities;
+
+    }
+
     public static void main(String[] args) {
         System.out.println("Student Activities : " + printStudentActivities());
+        System.out.println("Unique Student Activities : " + printUniqueStudentActivities());
+        System.out.println("Unique Student Activities Count: " + getStudentActivitiesCount());
     }
 }
